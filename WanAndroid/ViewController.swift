6f4aadea0a3131c51a23f6015fda580c6e859d9b
iossocket <avx302@gmail.com>
2019-04-14 19:58:37 +0800

    Init project

---
+++ WanAndroid/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WanAndroid
+//
+//  Created by ZHU XUELIANG on 14/4/19.
+//  Copyright © 2019 iossocket. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
